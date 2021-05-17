930f26fe1fed1c91df898964c138d91303b953b7
ymarquina <ymarquina@users.noreply.github.com>
2021-05-16 22:28:02 -0700

    Link database, add ejs and add css styling elements

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const app = express();
+
+app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost:27017/writingDB', {useNewUrlParser:true,useUnifiedTopology:true});
+
+const promptSchema = new mongoose.Schema({
+  category: String,
+  prompt:String
+});
+
+const Prompt = mongoose.model('Prompt', promptSchema);
+
+
+
+Prompt.find(function(err,prompts){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log(prompts);
+  }
+});
+
 
 
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.sendFile(__dirname + "/index.html")
+  res.render("index")
 })
+
 
 
 
