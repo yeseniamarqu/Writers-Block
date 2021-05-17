ae12b6acfc01e0630879462221fd8c4a829f773b
ymarquina <ymarquina@users.noreply.github.com>
2021-05-16 21:11:25 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html")
+})
+
+
+
+app.listen(3000, function(){
+  console.log("Listeing on port 3000")
+})
